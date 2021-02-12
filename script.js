6b551b7fcf95567e57362bd85a3bac5ea045a844
jjjjonathan <hornathanjon@gmail.com>
2021-02-12 16:04:59 -0600

    add dummy gameboard

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
+const game = (() => {
+
+})();
+
 const gameboard = (() => {
+  const board = ["X", "O", "X", "O", "O", null, "O", "X", "X"];
   const render = () => {
-    console.log("Console Gameboard!");
+    const domBoard = document.getElementById("gameboard");
+    board.forEach((element) => {
+      const domElement = document.createElement("div");
+      domElement.classList.add("gamepiece");
+      switch (element) {
+        case "X":
+          domElement.classList.add("x"); break;
+        case "O":
+          domElement.classList.add("o"); break;
+        case null:
+          domElement.classList.add("blank"); break;
+      }
+      domBoard.appendChild(domElement);
+    })
   };
+
   return {
     render,
   };
 })();
+
+const Player = (name, token) => {
+  const getName = () => name;
+  const getToken = () => token;
+
+  return {
+    getName,
+    getToken,
+  }
+}
 
 
 
