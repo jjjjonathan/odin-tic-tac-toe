878081dc9394e7bd26e2475ffd73bc35a40eb3c7
jjjjonathan <hornathanjon@gmail.com>
2021-02-13 11:55:18 -0600

    add menu

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const game = (() => {
-
-})();
-
 const gameboard = (() => {
   const board = ["X", "O", "X", "O", "O", null, "O", "X", "X"];
   const render = () => {
-    const domBoard = document.getElementById("gameboard");
+    const wrapper = document.getElementById("gameboard-wrapper");
+    const domBoard = document.createElement("div");
+    domBoard.id = "gameboard";
     board.forEach((element) => {
       const domElement = document.createElement("div");
       domElement.classList.add("gamepiece");
-      domElement.classList.add("hvr-rectangle-out")
+      domElement.classList.add("hvr-rectangle-out");
       switch (element) {
         case "X":
           domElement.classList.add("x"); break;
@@ -19,6 +17,7 @@ const gameboard = (() => {
           domElement.classList.add("blank"); break;
       }
       domBoard.appendChild(domElement);
+      wrapper.appendChild(domBoard);
     })
   };
 
@@ -27,20 +26,71 @@ const gameboard = (() => {
   };
 })();
 
-const Player = (name, token) => {
+const menu = (() => {
+  const render = (() => {
+
+    const textBox = (id, value) => {
+      const domMenu = document.getElementById("menu");
+      const domTextBox = document.createElement("input");
+      domTextBox.id = id;
+      domTextBox.name = id;
+      domTextBox.value = value;
+      domMenu.appendChild(domTextBox);
+    }
+
+    const button = (id, value) => {
+      const domMenu = document.getElementById("menu");
+      const domButton = document.createElement("button");
+      domButton.id = id;
+      domButton.textContent = value;
+      domMenu.appendChild(domButton);
+    }
+
+    return {
+      textBox,
+      button,
+    }
+
+  })();
+
+  const getPlayerNames = () => {
+    menu.render.textBox("player-x", "Player X");
+    menu.render.textBox("player-o", "Player O");
+    menu.render.button("submit-players", "Play!");
+    // return [playerX, playerO]
+  }
+
+  return {
+    getPlayerNames,
+    render,
+  };
+})();
+
+const Player = (name) => {
   const getName = () => name;
-  const getToken = () => token;
 
   return {
     getName,
-    getToken,
   }
-}
-
-
-
-const onload = () => {
-  gameboard.render();
 };
 
-document.addEventListener("DOMContentLoaded", onload);
+const game = (() => {
+  let playerNames, playerX, playerO;
+  
+
+
+  const begin = () => {
+    playerNames = menu.getPlayerNames()
+    playerX = Player("Jonny")
+    playerO = Player("Mom")
+  }
+
+
+  return {
+    begin,
+  }
+
+})();
+
+
+document.addEventListener("DOMContentLoaded", game.begin);
